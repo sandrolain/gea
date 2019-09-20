da30c1a1e50f47c4f75cbd29ed376d207189cd5f
Sandro Lain <xande05@gmail.com>
2019-09-20 06:53:02 +0200

    Refactorized Model and Collection classes

--- src/lib/Model.mjs
+++ src/lib/Model.mjs
@@ -4,7 +4,6 @@ export default class Model
 {
 	constructor(data = {})
 	{
-		this.idKey	= "id";
 		this.data	= new Map();
 
 		this.setData(data);
@@ -14,13 +13,15 @@ export default class Model
 	{
 		if(data instanceof Model)
 		{
-			data = data.toMap();
+			this.data = data.toMap();
 		}
-
-		forEach(data, (value, key) =>
+		else
 		{
-			this.data.set(key, value);
-		});
+			forEach(data, (value, key) =>
+			{
+				this.data.set(key, value);
+			});
+		}
 	}
 
 	get(key)
@@ -43,5 +44,189 @@ export default class Model
 		const data = mapToObject(this.data);
 
 		return JSON.stringify(data);
+	}
+
+	get id()
+	{
+		return this.get(this.constructor.idKey);
+	}
+
+	get idKey()
+	{
+		return this.constructor.idKey;
+	}
+
+	static get idKey()
+	{
+		return "id";
+	}
+
+	static isValue(value)
+	{
+		return (value !== null && value !== undefined);
+	}
+}
+
+
+export class Collection
+{
+	constructor(items = [])
+	{
+		this.items		= [];
+		this.idIndex	= new Map();
+
+		this.setItems(items);
+	}
+
+	get model()
+	{
+		return Model;
+	}
+
+	cancelRebuildIdIndex()
+	{
+		if(this.rebuildIndexTO)
+		{
+			clearTimeout(this.rebuildIndexTO);
+
+			this.rebuildIndexTO = null;
+		}
+	}
+
+	requestRebuildIdIndex()
+	{
+		this.cancelRebuildIdIndex();
+
+		this.idIndex = null;
+
+		this.rebuildIndexTO = setTimeout(() =>
+		{
+			this.rebuildIdIndex();
+		}, 100);
+	}
+
+	rebuildIdIndex()
+	{
+		this.cancelRebuildIdIndex();
+
+		if(!this.idIndex)
+		{
+			this.idIndex = new Map();
+			const idKey	= this.model.idKey;
+
+			this.items.forEach((item) =>
+			{
+				const idValue = item.get(idKey);
+
+				if(Model.isValue(idValue))
+				{
+					this.idIndex.set(idValue, item);
+				}
+			});
+		}
+	}
+
+	setItems(items)
+	{
+		this.items = [];
+
+		this.appendItems(items);
+	}
+
+	appendItems(items)
+	{
+		forEach(items, (item) =>
+		{
+			this.items.push(this._toModel(item));
+		});
+
+		this.requestRebuildIdIndex();
+	}
+
+
+	getItemById(id)
+	{
+		this.rebuildIdIndex();
+
+		return this.idIndex.get(id);
+	}
+
+	setItemById(item)
+	{
+		this.rebuildIdIndex();
+
+		const oldItem = this.getItemById(item.id);
+
+		if(oldItem)
+		{
+			const index = this.items.indexOf(oldItem);
+
+			return this.setItemByIndex(index, item);
+		}
+
+		this.addItem(item);
+	}
+
+	getItemByIndex(index)
+	{
+		return this.items[+index];
+	}
+
+	setItemByIndex(index, item)
+	{
+		this.items[+index] = this._toModel(item);
+
+		this.requestRebuildIdIndex();
+	}
+
+	addItem(item)
+	{
+		this.items.push(this._toModel(item));
+
+		this.requestRebuildIdIndex();
+	}
+
+	filter(fn)
+	{
+		const items = this.items.slice(0).filter(fn);
+
+		return new this.constructor(items);
+	}
+
+	slice(start = 0, end)
+	{
+		const items = this.items.slice(start, end);
+
+		return new this.constructor(items);
+	}
+
+	sort(field, desc = false)
+	{
+		const items = this.items.slice(0);
+
+		items.sort((a, b) =>
+		{
+			const aVal = a.get(field);
+			const bVal = b.get(field);
+
+			if(aVal === bVal)
+			{
+				return 0;
+			}
+
+			if(desc)
+			{
+				return aVal > bVal ? -1 : 1;
+			}
+
+			return aVal < bVal ? -1 : 1;
+		});
+
+		return new this.constructor(items);
+	}
+
+	_toModel(data)
+	{
+		return new this.model(data);
 	}
 }
